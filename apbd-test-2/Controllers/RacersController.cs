6b68555a8d065eb6981d76584c458b23dd701040
IvanMazhara <s31385@pjwstk.edu.pl>
2025-06-09 18:20:48 +0200

    Controllers implementation

---
+++ apbd-test-2/Controllers/RacersController.cs
@@ -0,0 +1,35 @@
+﻿using apbd_test_2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_test_2.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RacersController : ControllerBase
+{
+    private readonly IDbService _dbService;
+
+    public RacersController(IDbService dbService)
+    {
+        _dbService = dbService;
+    }
+    
+    [HttpGet]
+    [Route("{id}/participations")]
+    public async Task<IActionResult> GetRacerParticipations(int racerId)
+    {
+        try
+        {
+            var result = await _dbService.GetRacerParticipations(racerId);
+            return Ok();
+        }
+        catch (InvalidOperationException ex)
+        {
+            return Conflict(new { message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return NotFound(new { message = ex.Message });
+        }
+    }
+}
---
+++ apbd-test-2/Controllers/TrackRacesController.cs
@@ -0,0 +1,28 @@
+﻿using apbd_test_2.Models.DTOs;
+using apbd_test_2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_test_2.Controllers;
+
+[ApiController]
+[Route("api/track-races")]
+public class TrackRacesController : ControllerBase
+{
+    private readonly IDbService _dbService;
+
+    public TrackRacesController(IDbService dbService)
+    {
+        _dbService = dbService;
+    }
+
+    [HttpPost]
+    [Route("/participants")]
+    public async Task<IActionResult> AddRacerParticipaion(AddRacerParticipationDto racerParticipationDto)
+    {
+        throw new NotImplementedException();
+        // try
+        // {
+        //     var result = _dbService.AddNewRaceRacerParticipation(racerParticipationDto);
+        // }
+    }
+}
